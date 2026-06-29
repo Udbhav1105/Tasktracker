8a77447f88dd06fd1e7cbdff57dc60deb70895b2
Udbhav1105 <udbhavmsr@gmail.com>
2026-06-29 20:00:03 +0530

    UI fixes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,39 +1,37 @@
 import { Link, useNavigate } from "react-router-dom";
 import TaskList from "../components/TaskList";
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 // import SearchBar from "../components/SearchBar";
 // import FilterBar from "../components/FilterBar";
 
-export default function Dashboard(){
-    const navigate=useNavigate()
-     const [tasks, settasks] = useState([])
-     const getcart=async() => {
-     let res=await axios.get('/api/tasks/get')
-     settasks(res.data.tasks)
- }
-    useEffect(()=>{
-     getcart()
-    }, [])
-   
-return(
+export default function Dashboard() {
+  const navigate = useNavigate();
+  const [tasks, settasks] = useState([]);
+  const getcart = async () => {
+    let res = await axios.get("/api/tasks/get");
+    settasks(res.data.tasks);
+  };
+  useEffect(() => {
+    getcart();
+  }, []);
 
-<div className="bg-slate-100 flex py-8 justify-center  items-start">
-
-<div className="max-w-7xl">
-<h1 className="text-4xl font-bold mb-8">
-Task Dashboard
-</h1>
-<TaskList tasks={tasks} getTasks={getcart} />
-</div>
-
- <button
- onClick={()=>{
-    navigate("/create")
- }}
- className="bg-blue-400 text-gray-950 rounded-full px-6 py-2 font-semibold text-xl">Create Task</button>
-</div>
-
-)
-
+  return (
+    <div className="flex flex-col justify-center items-center" >
+      <div className="flex justify-between py-4 px-4">
+        <h1 className="text-4xl font-bold mb-8">Task Dashboard</h1>
+      <button
+        onClick={() => {
+          navigate("/create");
+        }}
+        className="bg-blue-400 absolute right-10 cursor-pointer text-gray-950 rounded-full px-6 py-1 font-semibold text-xl"
+      >
+        Create Task
+      </button>
+      </div>
+      <div className="bg-slate-100 w-screen flex py-8 justify-center  items-start">
+          <TaskList tasks={tasks} getTasks={getcart} />
+      </div>
+    </div>
+  );
 }

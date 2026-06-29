297839010048137bde9b1d5f0e71ffa7e69c8bf7
Udbhav1105 <udbhavmsr@gmail.com>
2026-06-29 15:03:04 +0530

    same domain

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 
 import taskRoute from "./Routes/taskRoute.js";
 import connectDB from "./Models/db.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -15,6 +17,15 @@ app.use(express.json());
 connectDB()
 
 app.use("/api/tasks",taskRoute);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 app.listen(3000,()=>{
     console.log("Server Running");
